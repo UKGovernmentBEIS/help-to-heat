5bf448adcb8958eb44c0b5d6e6d390ae72ae692c
rickyp72 <2026055+rickyp72@users.noreply.github.com>
2023-05-31 11:24:39 +0100

    add robots.txt

--- portal/help_to_heat/urls.py
+++ portal/help_to_heat/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.static import serve
+from django.urls import include, path
+from django.views.generic.base import TemplateView
 from help_to_heat.frontdoor.urls import frontdoor_patterns
 from help_to_heat.portal.urls import api_patterns, portal_patterns
 
@@ -9,6 +9,7 @@ if settings.SHOW_FRONTDOOR:
     urlpatterns = [
         path("portal/", include((portal_patterns, "portal"))),
         path("", include((frontdoor_patterns, "frontdoor"))),
+        path("robots.txt", TemplateView.as_view(template_name="frontdoor/robots.txt", content_type="text/plain")),
     ]
 else:
     urlpatterns = [
@@ -19,5 +20,4 @@ else:
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_patterns)),
-    re_path(r'^robots\.txt$', serve, {'document_root': settings.STATIC_ROOT, 'path': "robots.txt"}),
 ] + urlpatterns
